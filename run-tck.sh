ac0d07641231e241d2902a2326c113c79f1d8a8b
Richard Zowalla <rzo1@apache.org>
2022-06-20 19:25:43 +0200

    Updates Setup with new Geronimo Mail version

--- run-tck.sh
+++ run-tck.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export TCKHOME=$(pwd)
 export JARPATH=$(pwd)
-export CLASSPATH=$JARPATH/geronimo-mail_2.1_mail-1.0.0-SNAPSHOT.jar:$JARPATH/geronimo-activation_2.0_spec-1.0.0.jar:$JARPATH/geronimo-mail_2.1_spec-1.0.0-M1.jar
+export CLASSPATH=$JARPATH/geronimo-mail_2.1_mail-1.0.1-SNAPSHOT.jar:$JARPATH/geronimo-activation_2.0_spec-1.0.0.jar:$JARPATH/geronimo-mail_2.1_spec-1.0.0-M1.jar
 
 ant run
